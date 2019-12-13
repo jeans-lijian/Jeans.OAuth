c2ffc011b780e4d42482559426ba75e72aa310fc
Jian Li <lijian15@cn.wilmar-intl.com>
2019-12-13 15:08:33 +0800

    sql

---
+++ OAuthSolution/OAuth.AuthorizationServer/App_Data/client.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select * from lijian.credentials
+LIMIT 0, 1000
+
+-- Date: 2019-12-13 15:07
+*/
+INSERT INTO `` (`Id`,`ClientId`,`ClientSecret`,`GrantTypeMode`) VALUES ('0A17B60E-4B77-403E-AF0F-05230881C5B2','jeans','123456','ClientCredentials');
+INSERT INTO `` (`Id`,`ClientId`,`ClientSecret`,`GrantTypeMode`) VALUES ('84B49F28-3405-4798-B9D0-17F4A1C40784','jeans','123456','Password');
---
+++ OAuthSolution/OAuth.AuthorizationServer/App_Data/t_user.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: select * from lijian.t_user
+LIMIT 0, 1000
+
+-- Date: 2019-12-13 15:07
+*/
+INSERT INTO `` (`Id`,`UserName`,`Password`) VALUES ('75CDC7B4-1A4F-4876-B156-5F67C791737D','admin','123456');
