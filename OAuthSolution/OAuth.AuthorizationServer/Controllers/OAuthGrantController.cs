e43f8b383717e12c862539adfd96933aa382415c
Jian Li <lijian15@cn.wilmar-intl.com>
2019-12-17 17:37:22 +0800

    bootcss

---
+++ OAuthSolution/OAuth.AuthorizationServer/Controllers/OAuthGrantController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OAuth.AuthorizationServer.Controllers
+{
+    /// <summary>
+    /// oauth 授权clientid clientsercet url 等配置
+    /// </summary>
+    public class OAuthGrantController : Controller
+    {
+        // GET: OAuthGrant
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
